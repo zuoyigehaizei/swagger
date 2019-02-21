11e8b007f39c7f18ef0d7a1a79df903570c6ee3d
mengxiangkai <876587802@qq.com>
2019-02-21 17:59:10 +0800

    pageHelper分页插件配置

--- src/main/java/com/example/demoswagger/DemoswaggerApplication.java
+++ src/main/java/com/example/demoswagger/DemoswaggerApplication.java
@@ -1,5 +1,6 @@
 package com.example.demoswagger;
 
+import com.github.pagehelper.PageHelper;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Properties;
+
 //@ServletComponentScan
 @SpringBootApplication   //相当于 3个注解
 //@EnableAutoConfiguration
@@ -17,6 +20,19 @@ public class DemoswaggerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoswaggerApplication.class, args);
+    }
+
+    //pageHelper分页配置
+    @Bean
+    public PageHelper getPageHelper(){
+        PageHelper pageHelper=new PageHelper();
+        Properties properties=new Properties();
+        properties.setProperty("helperDialect","mysql");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("supportMethodsArguments","true");
+        properties.setProperty("params","count=countSql");
+        pageHelper.setProperties(properties);
+        return pageHelper;
     }
 
 }
---
+++ src/test/java/com/example/demoswagger/PageHelperTest.java
@@ -0,0 +1,24 @@
+package com.example.demoswagger;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PageHelperTest {
+
+//    @Autowired
+//    private WorkflowStepMapper workflowStepMapper;
+//
+//    @Test
+//    public void test01() {
+//        PageHelper.startPage(1, 2);
+//        List<WorkflowStepInfo> lists = workflowStepMapper.getLists();
+//        PageInfo<WorkflowStepInfo> pageInfo = new PageInfo<>(lists);
+//    }
+}
